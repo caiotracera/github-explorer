1704d4e9ecd1ce1d57d1fa675243a8a3554f98b5
Caio Tracera <caio.tracera@vitta.me>
2021-10-01 02:40:26 -0300

    style: add global style

---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,26 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body,
+  input,
+  textarea,
+  select,
+  button {
+    font: 400 1rem Arial, Helvetica, sans-serif;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+`;
